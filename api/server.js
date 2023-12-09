2ebaa8ff4a4fb4ae4ef815aa484246eb1e738c37
Leonardo Denavito <jounandavito@gmail.com>
2023-12-09 21:12:27 +0700

    Changed API routes

--- api/server.js
+++ api/server.js
@@ -7,6 +7,6 @@ const app = express();
 app.use(express.json());
 
 // Routes
-app.use('/', routes);
+app.use('/api', routes);
 
 module.exports = app; // Mengexport app agar dapat digunakan oleh Vercel
